a2302f7f10e2fb6bc44b395ac161e8122b14a3a8
Volodymyr <vshevchenko@lohika.com>
2013-02-08 15:59:17 +0200

    1st change

--- spam/minmax.hpp
+++ spam/minmax.hpp
@@ -1,8 +1,3 @@
-//  (C) Copyright Herve Bronnimann 2004.
-//
-// Distributed under the Boost Software License, Version 1.0. (See accompanying
-// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
-
 /*
  Revision history:
    1 July 2004
@@ -42,6 +37,7 @@ namespace boost {
     return comp(b,a) ? make_tuple(cref(b),cref(a)) : make_tuple(cref(a),cref(b));
   }
 
-} // namespace boost
+} // namespace boost new change
+
 
 #endif // BOOST_ALGORITHM_MINMAX_HPP
